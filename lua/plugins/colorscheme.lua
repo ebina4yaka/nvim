3df9250f9956c86993506f75f81f2c7e7d43c0cb
shunsuke takii <ebina4yaka@protonmail.com>
2026-04-27 14:20:07 +0900

    modify highlight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,11 +8,11 @@ return {
         -- leave this setup function empty for default config
         -- or refer to the configuration section
         -- for configuration options
-        bold_vert_split = true, -- use bold vertical separators
+        bold_vert_split = false, -- use bold vertical separators
         dim_nc_background = false, -- dim 'non-current' window backgrounds
         disable_background = true, -- disable background
         disable_float_background = false, -- disable background for floats
-        disable_italics = false, -- disable italics
+        disable_italics = false, -- disabl:e italics
       })
     end,
 
@@ -26,5 +26,12 @@ return {
     opts = {
       colorscheme = "poimandres",
     },
+    config = function()
+      local guibg = "#AAFF00"
+      local guifg = "#000000"
+      vim.cmd("highlight LspReferenceRead  guibg=" .. guibg .. " guifg=" .. guifg)
+      vim.cmd("highlight LspReferenceWrite guibg=" .. guibg .. " guifg=" .. guifg)
+      vim.cmd("highlight LspReferenceText  guibg=" .. guibg .. " guifg=" .. guifg)
+    end,
   },
 }
